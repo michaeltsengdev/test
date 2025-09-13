8ffc466f1bd11c07194be0d3d4582195f1ece5f0
michaeltsengdev <michaeltsengdev@users.noreply.github.com>
2025-09-13 23:26:38 +0000

    Publish from SparkAI

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,10 @@ export const metadata: Metadata = {
     canonical: '/'
   },
   title: {
-    default: 'Nim - Personal website template',
-    template: '%s | Nim'
+    default: 'Michael Tseng - Personal website template',
+    template: '%s | Michael Tseng'
   },
-  description:  'Nim is a free and open-source personal website template built with Next.js 15, React 19 and Motion-Primitives.',
+  description:  'Michael Tseng is a free and open-source personal website template built with Next.js 15, React 19 and Motion-Primitives.',
 };
 
 const geist = Geist({
